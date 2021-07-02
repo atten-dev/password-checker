e0cab4e327241a2b7a67b20e8d7c2696ab617292
John Lima <atten.dev@gmail.com>
2021-07-01 20:25:26 -0700

    switch to hashmap impl

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@ use std::convert::TryFrom;
 use std::io::prelude::*;
 use std::fs::File;
 use std::env;
+use std::collections::HashMap;
 use sha1::{Sha1, Digest};
 
 struct TrieNode {
     // children: [Option<Box<TrieNode>>; 16]
-    children: Vec<Option<Box<TrieNode>>>
+    children: HashMap<u8, Box<TrieNode>>
 }
 
 impl TrieNode {
     fn new() -> TrieNode {
-        TrieNode { children: Vec::with_capacity(16usize) } //[None; 16] } 
+        TrieNode { children: Default::default() } //[None; 16] } 
     }
 }
 
@@ -35,19 +36,17 @@ impl Trie
 
     fn store_hash_recursive(node: &mut TrieNode, pw_remainder: &str) {
         if let Some(first_char) = pw_remainder.chars().next() {
-            let child_idx = usize::try_from(first_char.to_digit(16).unwrap()).unwrap();
-            if node.children.len() == 0 {
-                node.children.resize_with(16, Default::default);  
-            }
-            match node.children[child_idx] { 
-                None => {
-                    node.children[child_idx] = Some(Box::new(TrieNode::new()))
+            let child_idx = u8::try_from(first_char.to_digit(16).unwrap()).unwrap();
+
+            match node.children.contains_key(&child_idx) { 
+                false => {
+                    node.children.insert(child_idx, Box::new(TrieNode::new()));
                 }
-                Some(ref child) => {
+                true => {
                     
                 }
             }
-            Trie::store_hash_recursive(node.children[child_idx].as_mut().unwrap(), &pw_remainder[1..])
+            Trie::store_hash_recursive(node.children.get_mut(&child_idx).unwrap().as_mut(), &pw_remainder[1..])
         }
     }
 
@@ -74,16 +73,14 @@ impl Trie
 
     fn check_hash_recursive(node: &TrieNode, pw_hash_remainder: &str) -> bool{
         if let Some(first_char) = pw_hash_remainder.chars().next() {
-            let child_idx = usize::try_from(first_char.to_digit(16).unwrap()).unwrap();
-            if node.children.len() == 0 {
-                return false;
-            }
-            match node.children[child_idx] { 
-                None => {
+            let child_idx = u8::try_from(first_char.to_digit(16).unwrap()).unwrap();
+
+            match node.children.contains_key(&child_idx) { 
+                false => {
                     return false
                 }
-                Some(ref child) => {
-                    return Trie::check_hash_recursive(child, &pw_hash_remainder[1..])
+                true => {
+                    return Trie::check_hash_recursive(node.children.get(&child_idx).unwrap(), &pw_hash_remainder[1..])
                 }
             }
         }
@@ -136,14 +133,14 @@ mod tests {
     fn store_hash() {
         let mut trie = Trie::new();
         trie.store_hash("0");
-        assert!(trie.root.children[0].is_some());
+        assert!(trie.root.children.get(&0u8).is_some());
     }
 
     #[test]
     fn store_hash_two_char() {
         let mut trie = Trie::new();
         trie.store_hash("21");
-        assert!(trie.root.children[2].as_ref().unwrap().children[1].is_some());
+        assert!(trie.root.children.get(&2u8).as_ref().unwrap().children.get(&1u8).is_some());
     }
 
     #[test]
@@ -151,54 +148,54 @@ mod tests {
         let mut trie = Trie::new();
         trie.store_hash("21");
         trie.store_hash("12");
-        assert!(trie.root.children[1].as_ref().unwrap().children[2].is_some());
-        assert!(trie.root.children[2].as_ref().unwrap().children[1].is_some());
+        assert!(trie.root.children.get(&1u8).as_ref().unwrap().children.get(&2u8).is_some());
+        assert!(trie.root.children.get(&2u8).as_ref().unwrap().children.get(&1u8).is_some());
     }
 
     #[test]
     fn store_hash_full_hash() {
         let mut trie = Trie::new();
         trie.store_hash("5BAA61E4C9B93F3F0682250B6CF8331B7EE68FD8");
-        assert!(trie.root.children[5].as_ref().unwrap().
-                          children[11].as_ref().unwrap(). // B
-                          children[10].as_ref().unwrap(). // A
-                          children[10].as_ref().unwrap(). // A
-                          children[6].as_ref().unwrap().
-                          children[1].as_ref().unwrap().
-                          children[14].as_ref().unwrap(). // E
-                          children[4].as_ref().unwrap().
-                          children[12].as_ref().unwrap(). // C
-                          children[9].as_ref().unwrap().
-                          children[11].as_ref().unwrap(). // B
-                          children[9].as_ref().unwrap().
-                          children[3].as_ref().unwrap().
-                          children[15].as_ref().unwrap(). // F
-                          children[3].as_ref().unwrap().
-                          children[15].as_ref().unwrap(). // F
-                          children[0].as_ref().unwrap().
-                          children[6].as_ref().unwrap().
-                          children[8].as_ref().unwrap().
-                          children[2].as_ref().unwrap().
-                          children[2].as_ref().unwrap().
-                          children[5].as_ref().unwrap().
-                          children[0].as_ref().unwrap().
-                          children[11].as_ref().unwrap(). // B
-                          children[6].as_ref().unwrap().
-                          children[12].as_ref().unwrap(). // C
-                          children[15].as_ref().unwrap(). // F
-                          children[8].as_ref().unwrap().
-                          children[3].as_ref().unwrap().
-                          children[3].as_ref().unwrap().
-                          children[1].as_ref().unwrap().
-                          children[11].as_ref().unwrap(). // B
-                          children[7].as_ref().unwrap().
-                          children[14].as_ref().unwrap(). // E
-                          children[14].as_ref().unwrap(). // E
-                          children[6].as_ref().unwrap().
-                          children[8].as_ref().unwrap().
-                          children[15].as_ref().unwrap(). // F
-                          children[13].as_ref().unwrap(). // D
-                          children[8].is_some());
+        assert!(trie.root.children.get(&5u8).as_ref().unwrap().
+                          children.get(&11u8).as_ref().unwrap(). // B
+                          children.get(&10u8).as_ref().unwrap(). // A
+                          children.get(&10u8).as_ref().unwrap(). // A
+                          children.get(&6u8).as_ref().unwrap().
+                          children.get(&1u8).as_ref().unwrap().
+                          children.get(&14u8).as_ref().unwrap(). // E
+                          children.get(&4u8).as_ref().unwrap().
+                          children.get(&12u8).as_ref().unwrap(). // C
+                          children.get(&9u8).as_ref().unwrap().
+                          children.get(&11u8).as_ref().unwrap(). // B
+                          children.get(&9u8).as_ref().unwrap().
+                          children.get(&3u8).as_ref().unwrap().
+                          children.get(&15u8).as_ref().unwrap(). // F
+                          children.get(&3u8).as_ref().unwrap().
+                          children.get(&15u8).as_ref().unwrap(). // F
+                          children.get(&0u8).as_ref().unwrap().
+                          children.get(&6u8).as_ref().unwrap().
+                          children.get(&8u8).as_ref().unwrap().
+                          children.get(&2u8).as_ref().unwrap().
+                          children.get(&2u8).as_ref().unwrap().
+                          children.get(&5u8).as_ref().unwrap().
+                          children.get(&0u8).as_ref().unwrap().
+                          children.get(&11u8).as_ref().unwrap(). // B
+                          children.get(&6u8).as_ref().unwrap().
+                          children.get(&12u8).as_ref().unwrap(). // C
+                          children.get(&15u8).as_ref().unwrap(). // F
+                          children.get(&8u8).as_ref().unwrap().
+                          children.get(&3u8).as_ref().unwrap().
+                          children.get(&3u8).as_ref().unwrap().
+                          children.get(&1u8).as_ref().unwrap().
+                          children.get(&11u8).as_ref().unwrap(). // B
+                          children.get(&7u8).as_ref().unwrap().
+                          children.get(&14u8).as_ref().unwrap(). // E
+                          children.get(&14u8).as_ref().unwrap(). // E
+                          children.get(&6u8).as_ref().unwrap().
+                          children.get(&8u8).as_ref().unwrap().
+                          children.get(&15u8).as_ref().unwrap(). // F
+                          children.get(&13u8).as_ref().unwrap(). // D
+                          children.get(&8u8).is_some());
     }   
 
     #[test]
@@ -220,7 +217,7 @@ mod tests {
     #[test]
     fn load_hashes() {
         let mut trie = Trie::new();
-        trie.load_hashes("test_pws.txt");
+        trie.load_hashes("test_pws.txt").expect("Failed to load test vector");
         assert!(trie.check_password_exists("password"));
         assert!(!trie.check_password_exists("Password"));
     }
