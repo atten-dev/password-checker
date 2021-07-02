7b3581c501406bde3fddfaff4d6f6af1b6034503
John Lima <atten.dev@gmail.com>
2021-07-02 12:59:53 -0700

    implement indexing strategy

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,38 @@
-use std::io::{self, BufReader};
+use std::io::{self, BufRead, BufReader, Write, BufWriter, Seek, SeekFrom};
 use std::convert::TryFrom;
-use std::io::prelude::*;
 use std::fs::File;
-use std::env;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use sha1::{Sha1, Digest};
-use generic_array::{ArrayLength, GenericArray};
 use hex::FromHex;
+use clap::{Arg, App};
 
-struct TrieNode {
-    // children: [Option<Box<TrieNode>>; 16]
-    children: HashMap<u8, Box<TrieNode>>
-}
-
-impl TrieNode {
-    fn new() -> TrieNode {
-        TrieNode { children: Default::default() } //[None; 16] } 
-    }
-}
-
-// impl Default for TrieNode {
-//     fn default() -> TrieNode {
-//         TrieNode::new()
-//     }
-// }
-
-struct Trie
+struct PasswordChecker
 {
-    root: TrieNode,
-    // hashes: Vec<GenericArray<u8,generic_array::typenum::U18>>
-    // hashes: Vec<[u8; 20]>
+    hash_pos_map: BTreeMap<String, u64>,
+    hash_path: String
 }
 
-impl Trie
+impl PasswordChecker
 {
-    fn new() -> Trie {
-        Trie { root: TrieNode::new() }
+    fn new(hash_path: &str) -> PasswordChecker {
+        PasswordChecker { hash_pos_map: Default::default(), hash_path: hash_path.to_string() }
     }
 
-    fn store_hash_recursive(node: &mut TrieNode, hash_remainder: &[u8]) {
-        if let Some(first_byte) = hash_remainder.first() {
-            if !node.children.contains_key(&first_byte) { 
-                node.children.insert(*first_byte, Box::new(TrieNode::new()));
-            }
-            Trie::store_hash_recursive(node.children.get_mut(&first_byte).unwrap().as_mut(), &hash_remainder[1..])
-        }
+    fn store_hash(&mut self, hash: &str, pos: u64) {
+        self.hash_pos_map.insert(hash.to_string(), pos);
     }
 
-    fn store_hash_bin(&mut self, hash: [u8; 20]) { //GenericArray<u8, generic_array::typenum::U18>) {
-        Trie::store_hash_recursive(&mut self.root, &hash)
-    }
-
-    fn store_hash(&mut self, hash: &str) {
-        if hash.len() < 2 { return; }
-        let bin_hash = <[u8; 20] as FromHex>::from_hex(hash).expect("Hex to binary conversion failed");
-        self.store_hash_bin(bin_hash);
-    }
-
-    fn load_hashes(&mut self, path: &str) -> io::Result<()> {
+    fn load_index(&mut self, path: &str) -> io::Result<()> {
         let f = File::open(path)?;
         let f = BufReader::new(f);
 
         let mut counter : u64 = 0;
         for line in f.lines() {
-            self.store_hash(line.unwrap().split(":").next().unwrap());
+            let line = line.unwrap();
+            let vec = line.trim().split(":").take(2).collect::<Vec<&str>>();
+            let hash = vec[0];
+            let pos = vec[1].parse::<u64>().unwrap();
+            self.store_hash(hash, pos);
             counter+=1;
             if counter % 1000 == 0 {
                 println!("Loaded {} hashes so far", counter);
@@ -73,26 +42,40 @@ impl Trie
         Ok(())
     }
 
-    fn check_hash_recursive(node: &TrieNode, hash_remainder: &[u8]) -> bool{
-        if let Some(first_byte) = hash_remainder.first() {
-            if node.children.contains_key(&first_byte) { 
-                Trie::check_hash_recursive(node.children.get(&first_byte).unwrap(), &hash_remainder[1..])
-            }
-            else {
-                false 
-            }
-        }
-        else {
-            true
+    fn get_file_pos(&self, pw_hash: &str) -> u64 {
+        match self.hash_pos_map.range(pw_hash.to_string()..).next() {
+            Some(kv) => *kv.1,
+            None => 0
         }
     }
 
-    fn check_hash_exists(&self, pw_hash: &str) -> bool {
-        let bin_hash = <[u8; 20] as FromHex>::from_hex(pw_hash).expect("Hex to binary conversion failed");
-        Trie::check_hash_recursive(&self.root, &bin_hash)
+    fn check_hash_exists(&self, pw_hash: &str) -> u64 {
+        let pos = self.get_file_pos(pw_hash);
+        let f = File::open(&self.hash_path).expect("Failed to open hash file");
+        let mut f = BufReader::new(f);
+        f.seek(SeekFrom::Start(pos)).expect("Failed to seek");
+        for _ in 0..100 {
+            let mut line = String::new(); 
+            let res = f.read_line(&mut line);
+            match res
+            {
+                Ok(0) => { break; }
+                Ok(_) => {
+                    let vec = line.trim().split(":").take(2).collect::<Vec<&str>>();
+                    let hash = vec[0];
+                    let count = vec[1].parse::<u64>().unwrap();
+                    if hash == pw_hash {
+                        return count;
+                    }
+                }
+                Err(e) => { panic!("Failed to read line of hash file: {}", e); }
+            }
+
+        }
+        0
     }
 
-    fn check_password_exists(&self, pw: &str) -> bool {
+    fn check_password_exists(&self, pw: &str) -> u64 {
         let mut hasher = Sha1::new();
         hasher.update(pw);
         let result = hasher.finalize();
@@ -102,27 +85,92 @@ impl Trie
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    assert!(args.len() > 1);
+fn create_index(hash_path: &str, index_path: &str) -> io::Result<()> {
+    let mut f = BufReader::new(File::open(hash_path)?);
+    let mut out_f = BufWriter::new(File::create(index_path)?);
 
-    println!("Loading hashes from {}.", args[1]);
-    let mut hash_trie = Trie::new();
-    hash_trie.load_hashes(&args[1]).expect("Failed to open hash file");
+    let mut line = String::new();
+    let mut counter : u64 = 0;
     loop {
-        println!("Please enter the password you would like to check:");
-        let mut pw = String::new();
-        io::stdin().read_line(&mut pw).expect("Failed to read line");
-        let pw = pw.trim();
-        println!("You entered: {}", pw);
-        let pw_exists = hash_trie.check_password_exists(pw);
-        match pw_exists {
-            true => println!("Password is in database"),
-            false => println!("Password is not in database")
+        let before_pos = f.stream_position()?;
+        match f.read_line(&mut line) {
+            Ok(0) => { break; } // Reprs EOF reached
+            Ok(_) => {
+                if counter % 100 == 0 {
+                    let hash = line.split(":").next().unwrap();
+                    let line_to_write = format!("{}:{}\n", hash, before_pos);
+                    out_f.write_all(line_to_write.as_bytes())?
+                }
+            }
+            Err(_) => {
+                break;
+            }
         }
-        println!();
+        line.clear();
+        counter += 1;
+        if counter % 1000 == 0 {
+            println!("Indexed {} hashes", counter);
+        }
     }
+    // Write the last line to the map to make searching easier
+    // let hash = line.split(":").next().unwrap();
+    // let line_to_write = format!("{}:{}\n", hash, f.stream_position()?);
+    // out_f.write_all(line_to_write.as_bytes())?;
+    out_f.flush()?;
+    Ok(())
 }
+
+fn main() {
+    let matches = App::new("My Test Program")
+        .version("0.1.0")
+        .author("John Lima <atten.dev@gmail.com>")
+        .about("Checks passwords against haveIbeenpwned's password list")
+        .arg(Arg::with_name("hash_file")
+                .short("h")
+                .long("hash-file")
+                .takes_value(true)
+                .required(true)
+                .help("List of password hashes"))
+        .arg(Arg::with_name("index_file")
+                .short("i")
+                .long("index-file")
+                .takes_value(true)
+                .required(true)
+                .help("Name of index file"))
+        .arg(Arg::with_name("create_index")
+                .short("c")
+                .long("create-index")
+                .takes_value(false)
+                .help("Create index file instead of running checking"))
+        .get_matches();
+
+    let hash_path = matches.value_of("hash_file").unwrap();
+    let index_path = matches.value_of("index_file").unwrap();
+
+    match matches.is_present("create_index") {
+        true => {
+            println!("Creating index at {} from {}", index_path, hash_path);
+            create_index(hash_path, index_path).expect("Failed to create index");
+        }
+        false => {
+            println!("Loading index file from {}", index_path);
+            let mut hash_trie = PasswordChecker::new(hash_path);
+            hash_trie.load_index(index_path).expect("Failed to open index file");
+            loop {
+                println!("Please enter the password you would like to check:");
+                let mut pw = String::new();
+                io::stdin().read_line(&mut pw).expect("Failed to read line");
+                let pw = pw.trim();
+                println!("You entered: {}", pw);
+                let pw_exists = hash_trie.check_password_exists(pw);
+                match pw_exists {
+                    0 => println!("Password is not in database"),
+                    i => println!("Password is in database {} times", i)
+                }
+                println!();
+            }
+        }
+    }}
 
 #[cfg(test)]
 mod tests {
@@ -130,9 +178,9 @@ mod tests {
 
     #[test]
     fn store_hash() {
-        let mut trie = Trie::new();
-        trie.store_hash("0");
-        assert!(trie.root.children.get(&0u8).is_some());
+        let mut pc = PasswordChecker::new("");
+        pc.store_hash("0", 1);
+        assert_eq!(1, pc.check_hash_exists("0"));
     }
 
     #[test]
